44fa9ef909bc69a1a212928cdbd3f1374324d9a3
celestemayumi <celestetanakaa@gmail.com>
2025-02-14 09:44:19 -0300

    aula 1

--- src/main/java/br/com/fiap/spring_mvc/repository/LivroRepository.java
+++ src/main/java/br/com/fiap/spring_mvc/repository/LivroRepository.java
@@ -1,5 +1,8 @@
 package br.com.fiap.spring_mvc.repository;
 
-public interface LivroRepository {
+import br.com.fiap.spring_mvc.model.Livro;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LivroRepository extends JpaRepository<Livro, Long> {
 
 }
